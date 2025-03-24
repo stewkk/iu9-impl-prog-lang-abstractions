4242ce82f5928d1efa568baded0a459c40094b1e
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-03-24 17:11:25 +0300

    Impl main

--- stack-assembly-interpreter/src/main.rs
+++ stack-assembly-interpreter/src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, fs};
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_paths = &args[1..];
+
+    run(&file_paths).unwrap();
+}
+
+fn run(file_paths: &[String]) -> Result<(), Box<dyn Error>> {
+    let contents: Result<Vec<_>, _> = file_paths.iter().map(fs::read_to_string).collect();
+
+    println!("Contents: |{}|", contents?.concat());
+
+    Ok(())
 }
