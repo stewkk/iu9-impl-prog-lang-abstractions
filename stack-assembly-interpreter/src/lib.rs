de96134c1294e48db380aeb21648646d25b7e11b
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-03-25 22:43:29 +0300

    Impl first assembly pass

--- stack-assembly-interpreter/src/lib.rs
+++ stack-assembly-interpreter/src/lib.rs
@@ -1,9 +1,12 @@
 mod logic;
 mod models;
 
-use std::{error, fs};
+use std::{collections::HashMap, error, fs};
+
+use anyhow::{bail, Result};
 
 use logic::tokenize;
+use models::token::Token;
 
 pub fn run(file_paths: &[String]) -> Result<(), Box<dyn error::Error>> {
     let contents: Result<Vec<_>, _> = file_paths.iter().map(fs::read_to_string).collect();
@@ -13,6 +16,43 @@ pub fn run(file_paths: &[String]) -> Result<(), Box<dyn error::Error>> {
     Ok(())
 }
 
+type CommandHandler = fn() -> ();
+
+struct Command<'a> {
+    code: i32,
+    mnemonic: &'a str,
+    handler: CommandHandler,
+}
+
+fn AddHandler() -> () {}
+
+fn SubHandler() -> () {}
+
+static COMMANDS: &[Command] = &[
+    Command{code: -1, mnemonic: "ADD", handler: AddHandler},
+    Command{code: -2, mnemonic: "SUB", handler: SubHandler},
+];
+
+fn get_default_labels() -> HashMap<&'static str, i32> {
+    COMMANDS.iter().map(|x| (x.mnemonic, x.code)).collect()
+}
+
+fn get_labels(tokens: &[Token]) -> Result<HashMap<&str, i32>> {
+    let mut current = 256;
+    let mut labels = get_default_labels();
+    for token in tokens {
+        if let Token::Declaration(decl) = token {
+            if let Some(_) = labels.insert(decl, current) {
+                bail!("label declared twice: {decl}");
+            }
+        } else {
+            current += 1;
+        }
+    }
+    labels.insert("PROGRAM_SIZE", current);
+    Ok(labels)
+}
+
 fn _assembly(_text: &str) -> Vec<i32> {
     vec![]
 }
@@ -20,6 +60,32 @@ fn _assembly(_text: &str) -> Vec<i32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn calculates_labels_map() {
+        let tokens = vec![
+            Token::Integer(-40),
+            Token::Ident("_Loop".to_string()),
+            Token::Declaration("a1".to_string()),
+            Token::Ident("HALT".to_string()),
+            Token::Ident("_Read_number_".to_string()),
+            Token::Ident("a1".to_string()),
+            Token::Integer(123),
+            Token::Integer(1234),
+            Token::Ident("PROGRAM_SIZE".to_string()),
+            Token::Declaration("_Loop".to_string()),
+            Token::Declaration("_Read_number_".to_string()),
+        ];
+        let mut expected = get_default_labels();
+        expected.insert("_Loop", 264);
+        expected.insert("a1", 258);
+        expected.insert("_Read_number_", 264);
+        expected.insert("PROGRAM_SIZE", 264);
+
+        let got = get_labels(&tokens);
+
+        assert_eq!(got.unwrap(), expected)
+    }
 
     #[ignore = "not implemented"]
     #[test]
