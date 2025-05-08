14bb0b4df63a53e8bb9a8ddb2d71eeb34455f5b8
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-05-08 15:12:58 +0300

    Test cli args

--- lab2/tests/task1.py
+++ lab2/tests/task1.py
@@ -2,15 +2,23 @@
 
 import pytest
 import subprocess
+import shlex
 
-def test_example():
+
+@pytest.mark.parametrize("args, expected_stdout, expected_stderr",
+                         [
+                             ("test_single.txt", "test_single.txt", ""),
+                         ])
+def test_task1(args, expected_stdout, expected_stderr):
     process = subprocess.run(
         [
             "refal/refgo",
             "task1.rsl",
-        ],
+        ]+shlex.split(args),
         capture_output=True,
         text=True,
     )
-    output = process.stdout.rstrip()
-    assert output == "Hello"
+
+    output = process.stdout.rstrip(), process.stderr.rstrip()
+
+    assert output == (expected_stdout, expected_stderr)
