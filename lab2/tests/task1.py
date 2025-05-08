013c851bc413ad74bec93305c236a8fa9d3d40c7
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-05-08 14:07:36 +0300

    Add test

--- lab2/tests/task1.py
+++ lab2/tests/task1.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+import pytest
+import subprocess
+
+def test_example():
+    process = subprocess.run(
+        [
+            "refal/refgo",
+            "task1.rsl",
+        ],
+        capture_output=True,
+        text=True,
+    )
+    output = process.stdout.rstrip()
+    assert output == "Hello"
