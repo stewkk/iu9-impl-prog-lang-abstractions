1abff934e80d209a1074326e322f24afb9444a8e
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-05-13 15:49:52 +0300

    Add tests

--- lab2/tests/task2.py
+++ lab2/tests/task2.py
@@ -7,6 +7,14 @@ import subprocess
 
 TMP = "/tmp/refal_test_task2.txt"
 
+"""
+((((A "/" B) "-" C) "*" (D "+" E)) "/" F)
+((((A "-" (C "*" B)) "/" B) "*" (D "+" E)) "/" F)
+((((A "-" (C "*" B)) "/" B) "*" (D "+" E)) "/" F)
+((((A "-" (C "*" B)) "/" B) "*" (D "+" E)) "/" F)
+((((A "-" (C "*" B)) "*" (D "+" E)) "/" B) "/" F)
+(((A "-" (C "*" B)) "*" (D "+" E)) "/" (B "*" F))
+"""
 
 @pytest.mark.parametrize("stdin, expected_stdout",
                          [
@@ -15,7 +23,15 @@ TMP = "/tmp/refal_test_task2.txt"
                              ("""(((((a "*" b) "*" c) "+" (d "*" e)) "+" (f "*" g)) "*" h)""",
                               "((((a * b )* (c * h ))+ (d * (e * h )))+ (f * (g * h )))\n"),
                              ("""((a "/" b) "/" (c "/" d))""",
-                              "((a * d )/ (c * b ))\n")
+                              "((a * d )/ (c * b ))\n"),
+                             ("""((A "/" B) "-" C)""",
+                              "((A - (C * B ))/ B )\n"),
+                             ("""(((A "/" B) "-" C) "*" D)""",
+                              "(((A * D )- (C * (B * D )))/ B )\n"),
+                             ("""(((A "/" B) "-" C) "*" (D "+" E))""",
+                              "((((D * A )+ (E * A ))- ((D * (B * C ))+ (E * (B * C ))))/ B )\n"),
+                             ("""((((A "/" B) "-" C) "*" (D "+" E)) "/" F)""",
+                              "((((D * A )+ (E * A ))- ((D * (B * C ))+ (E * (B * C ))))/ (B * F ))\n"),
                          ])
 def test_task1(stdin, expected_stdout):
     with open(TMP, 'w') as f: f.write(stdin)
